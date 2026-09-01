b9321239690e9e131f08f912835af01344a52fa3
mahadib2b <mahadib2b@gmail.com>
2026-09-01 23:51:07 +0600

    Fix build.gradle.kts syntax errors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,37 +1,34 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    // আপনার প্যাকেজ নেম এখানে থাকবে
-    namespace "com.example.flashlight_app"
-    compileSdkVersion flutter.compileSdkVersion
-            ndkVersion flutter.ndkVersion
+    namespace = "com.example.flashlight_app"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
-            compileOptions {
-                sourceCompatibility JavaVersion.VERSION_1_8
-                        targetCompatibility JavaVersion.VERSION_1_8
-            }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 
     kotlinOptions {
-        jvmTarget = '1.8'
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
-        // এখানে আপনার applicationId থাকতে হবে
-        applicationId "com.example.flashlight_app"
-        minSdkVersion 21
-        targetSdkVersion flutter.targetSdkVersion
-                versionCode flutterVersionCode.toInteger()
-        versionName flutterVersionName
+        applicationId = "com.example.flashlight_app"
+        minSdk = 21
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            signingConfig signingConfigs.debug
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
